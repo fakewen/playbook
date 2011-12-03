ccc7c9e04a30d59da6e76623ca249521bd757f89
Aiko Lin <aiko0403@gmail.com>
2011-12-03 19:13:13 +0800

    add

--- PlayBook/src/ntu/mpp/proj/PlayBookActivity.java
+++ PlayBook/src/ntu/mpp/proj/PlayBookActivity.java
@@ -1,12 +1,12 @@
 package ntu.mpp.proj;
-//asdfasghshe
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-asdfasghshesdsd
+
 public class PlayBookActivity extends Activity {
     /** Called when the activity is first created. */
     Button bt1,bt2;
